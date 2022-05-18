cc1120c8b150cd298b9f2f3bc83efa233e13562a
Danielle Kensy <kensydanielle@gmail.com>
2022-05-18 13:07:28 -0300

    metodo de criação e busca estudante

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { app } from "./app";
 import createClass from "./endpoints/createClass";
 import createInstructor from "./endpoints/createInstructor";
+import createStudent from "./endpoints/createStudent";
+import getStudents from "./endpoints/getStudents";
+
+app.get("/student", getStudents)
 
 app.post("/class", createClass)
+
 app.post("/instructor", createInstructor)
+
+app.post("/student", createStudent)
