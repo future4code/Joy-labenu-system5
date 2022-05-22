687fcad56b20d4c76db112491b53b388c83ae160
LucianoRib5 <luciano.eng.soft@outlook.com>
2022-05-22 20:42:20 -0300

    Validações 'changeStudent'

--- src/constants/errors.ts
+++ src/constants/errors.ts
@@ -3,6 +3,7 @@ export const errors: {[key: string]: {errorCode: number, message: string}} = {
     CLASS_NOT_FOUND:{errorCode: 422, message: "Turma não encontrada!"},
     SAME_CLASS:{errorCode: 404, message: "A turma informada é a mesma que a turma atual!"},
     INSTRUCTOR_NOT_FOUND:{errorCode: 404, message: "Instrutor não encontrado!"},
+    STUDENT_NOT_FOUND:{errorCode: 404, message: "Estudante não encontrado!"},
     INVALID_TYPE_MODULE: {errorCode: 400, message: "Tipo inválido informado, verifique a documentação!"},
     INVALID_MODULE: {errorCode: 400, message: "O módulo deve ser menor ou igual a 6!"},
     OUTDATED_MODULE: {errorCode: 400, message: "O módulo precisa ser maior que módulo atual!"},
--- src/data/updateStudent.ts
+++ src/data/updateStudent.ts
@@ -1,14 +1,10 @@
 import connection from "../data/connection";
 
-//função para alterar um instrutor
-
 export default async function updateStudent(
-      id: string,
-      class_id: string
-    ) {
-      await connection("student")
-      .where({id})
-      .update({class_id})
-      
-    }
-      
+  id: string,
+  class_id: string
+) {
+  await connection("student")
+    .where('id', id)
+    .update('class_id', class_id);
+}
